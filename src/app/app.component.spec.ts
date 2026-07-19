de656f50961b8bdf7baf37375e77634eeadc95cf
uzielB <utti222033@utvco.edu.mx>
2026-07-18 23:54:43 -0600

    Agrega componentes de encabezado y pie de página en «app.component.ts» y actualiza las pruebas para verificar su renderizado

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,11 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ecommerfront');
   });
 
-  it('should render title', () => {
+  it('should render the header and footer', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ecommerfront');
+    expect(compiled.querySelector('app-header')).toBeTruthy();
+    expect(compiled.querySelector('app-footer')).toBeTruthy();
   });
 });
