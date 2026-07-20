5b0f0ce7bff6ef5e97cd022e230a612cd763b8cb
uzielB <utti222033@utvco.edu.mx>
2026-07-19 22:06:52 -0600

    Agrega componente Pedidos con su plantilla HTML

---
+++ src/app/features/vendedor/pedidos/pedidos.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pedidos',
+  standalone: true,
+  imports: [],
+  templateUrl: './pedidos.component.html'
+})
+export class PedidosComponent {}
