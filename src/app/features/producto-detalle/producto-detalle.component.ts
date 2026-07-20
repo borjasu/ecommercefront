64a1d79153618f662a0b66d8781ec905ddcf97d5
uzielB <utti222033@utvco.edu.mx>
2026-07-19 22:06:35 -0600

    Agrega componente ProductoDetalle con su plantilla HTML

---
+++ src/app/features/producto-detalle/producto-detalle.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-detalle',
+  standalone: true,
+  imports: [],
+  templateUrl: './producto-detalle.component.html'
+})
+export class ProductoDetalleComponent {}
