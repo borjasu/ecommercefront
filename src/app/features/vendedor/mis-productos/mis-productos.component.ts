46fcc14ee303be2455d09666c08aa5d4a8bc551f
uzielB <utti222033@utvco.edu.mx>
2026-07-19 22:06:45 -0600

    Agrega componentes Dashboard y Mis Productos con sus plantillas HTML

---
+++ src/app/features/vendedor/mis-productos/mis-productos.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mis-productos',
+  standalone: true,
+  imports: [],
+  templateUrl: './mis-productos.component.html'
+})
+export class MisProductosComponent {}
