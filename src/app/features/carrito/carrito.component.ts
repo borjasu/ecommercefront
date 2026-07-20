36b20da8a736bad5ff132172b6d4cefbe977549e
uzielB <utti222033@utvco.edu.mx>
2026-07-19 22:06:16 -0600

    Agrega componente Carrito con su plantilla HTML

---
+++ src/app/features/carrito/carrito.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrito',
+  standalone: true,
+  imports: [],
+  templateUrl: './carrito.component.html'
+})
+export class CarritoComponent {}
