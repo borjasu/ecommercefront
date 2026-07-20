fbe99f47023681a7124592b122220a5fc859fa51
uzielB <utti222033@utvco.edu.mx>
2026-07-19 22:07:03 -0600

    Actualiza el componente Footer: mejora la navegación y agrega enlaces a redes sociales

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-  readonly anio = 2026;
+  readonly anio = new Date().getFullYear();
+
+  readonly redesSociales = ['Instagram', 'Facebook', 'TikTok'];
 }
